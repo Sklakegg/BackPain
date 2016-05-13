d8da81d662c5613cf118553ad56578ce5ac29b08
Sklakegg <sklakegg@ee.oulu.fi>
2016-05-13 13:03:35 +0300

    Pictures.

--- app/src/main/java/android/com/backpain/StartActivity.java
+++ app/src/main/java/android/com/backpain/StartActivity.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
 
     TextView motivational;
     TextView information;
+    ImageView akatemia;
+    ImageView ubicomp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,8 @@ public class StartActivity extends AppCompatActivity {
 
         motivational = (TextView)findViewById(R.id.motivation);
         information = (TextView)findViewById(R.id.informationTextView);
+        akatemia = (ImageView)findViewById(R.id.imageView);
+        ubicomp = (ImageView)findViewById(R.id.imageView2);
 
         motivational.setText(
                   "•    This is a motivational text box \n"
@@ -29,6 +35,8 @@ public class StartActivity extends AppCompatActivity {
                 + "•    ..and science! (redirect to web interface, at least initially)");
 
         information.setText("Logos / contact details / link to online interface (kipuriihi.org)");
+        akatemia.setImageResource(R.mipmap.ic_akatemia);
+        ubicomp.setImageResource(R.drawable.ic_ubicmp);
     }
 
     public void onStartClick(View V) {
